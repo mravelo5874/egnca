ff7e94609d5b4ee81c86de50a447b0c733ac3c38
Marco Ravelo <mravelo5874@gmail.com>
2024-09-11 23:03:46 -0500

    trying to fix missing 'torch' module

--- condor/create_venv.sh
+++ condor/create_venv.sh
@@ -4,5 +4,6 @@ source ~/.bashrc
 cd /u/marco/github/egnca
 python3 -m venv venv
 source venv/bin/activate
+pip install --no-cache-dir torch
 pip install --no-cache-dir -r condor/requirements.txt
 echo "finished creating venv..."
