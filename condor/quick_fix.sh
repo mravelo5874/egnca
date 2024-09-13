7a9c3c00f86378d24d9392444e990ed557f2b306
Marco Ravelo <mravelo5874@gmail.com>
2024-09-12 20:17:54 -0500

    maybe this works?

--- condor/quick_fix.sh
+++ condor/quick_fix.sh
@@ -1,4 +1,5 @@
 eval "$('/u/marco/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 conda activate /u/marco/miniconda3/envs/egnca-env
 pip uninstall pytorch_lightning
+pip install hydra-core --upgrade
 pip install pytorch_lightning==1.7.3
