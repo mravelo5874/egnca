03fc2e41d33f7d42595abb363a831a5e683a37cd
Marco Ravelo <mravelo5874@gmail.com>
2024-09-12 20:30:51 -0500

    changed pytorch lightning version in quick_fix.sh

--- condor/quick_fix.sh
+++ condor/quick_fix.sh
@@ -2,4 +2,4 @@ eval "$('/u/marco/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 conda activate /u/marco/miniconda3/envs/egnca-env
 pip uninstall pytorch_lightning
 pip install hydra-core --upgrade
-pip install pytorch_lightning==1.7.3
+pip install pytorch_lightning==2.4.0
