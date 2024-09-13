868160c1c0380442a54ff4ccb56dd774534a20ff
Marco Ravelo <mravelo5874@gmail.com>
2024-09-12 19:05:25 -0500

    fixed train script to use miniconda

--- condor/train-fixed-target/train.sh
+++ condor/train-fixed-target/train.sh
@@ -1,5 +1,6 @@
 #!/lusr/bin/bash
 source ~/.bashrc
 cd /u/marco/github/egnca
-source venv/bin/activate
+eval "$('/u/marco/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+conda activate /u/marco/miniconda3/envs/egnca-env
 python3 -m trainers.geometric_graph -ds Bunny -sdg 0.05 -rdg 1.0 -bsc 8
