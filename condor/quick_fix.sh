6c89b275e9fbc1424a4132df1ac73cb80965c629
Marco Ravelo <mravelo5874@gmail.com>
2024-09-12 20:12:50 -0500

    trying to fix issue with training with quick_fix script

--- condor/quick_fix.sh
+++ condor/quick_fix.sh
@@ -1,4 +1,4 @@
 eval "$('/u/marco/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 conda activate /u/marco/miniconda3/envs/egnca-env
-pip install tensorboard
-pip install tensorboardX
+pip uninstall pytorch_lightning
+pip install pytorch_lightning==1.7.3
