179d5ac2964208fc55855df8b6a1950dbf610a7e
Marco Ravelo <mravelo5874@gmail.com>
2024-09-23 22:43:57 -0500

    might have figured it out....

--- condor/train-fixed-target/train.sh
+++ condor/train-fixed-target/train.sh
@@ -4,6 +4,6 @@ cd /u/marco/github/egnca
 eval "$('/u/marco/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 conda activate /u/marco/miniconda3/envs/egnca-env
 # python -m trainers.geometric_graph -ds Cube   -sdg 0.05 -rdg 1.0 -bsc 0 16
-python -m trainers.geometric_graph -ds Line -sdg 0.05 -rdg 1.0 -bsc 0 2
+python -m trainers.geometric_graph -ds Line -sdg 0.05 -rdg 1.0 -bsc 0 3
 # python3 -m trainers.geometric_graph -ds Bunny -sdg 0.05 -rdg 1.0 -bsc 4
 # CUDA_LAUNCH_BLOCKING=1 NCCL_DEBUG=INFO NCCL_DEBUG_SUBSYS=ALL NCCL_DEBUG_SUBSYS=ALL 
